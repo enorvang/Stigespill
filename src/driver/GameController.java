761f7a5564a68073c285de88407efb7e18545b63
enorvang <espen.norvang@gmail.com>
2020-01-28 16:41:18 +0100

    lagt til en egen klasse som velger spillere

--- src/driver/GameController.java
+++ src/driver/GameController.java
@@ -14,32 +14,12 @@ public class GameController {
     public static void main(String[] args) {
         Stigespill spill = new Stigespill();
         Brett brett = spill.getBrett();
-        ArrayList<Spiller> spillere = new ArrayList<>();
 
-        String[] muligeAntallSpillere = {"2", "3", "4"};
-        ArrayList<String> muligeFarger = new ArrayList<>(Arrays.asList("Rosa", "Sort", "Gul", "Brun"));
+        SpillerVelger sv = new SpillerVelger(spill, brett);
 
-        int valgtAntallSpillere = JOptionPane.showOptionDialog(null, "Velg antall spillere", "Antall spillere",
-                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, muligeAntallSpillere, muligeAntallSpillere[0]);
+        sv.velgSpillere();
 
-        int antallSpillere = Integer.parseInt(muligeAntallSpillere[valgtAntallSpillere]);
-
-        for (int i = 0; i < antallSpillere; i++) {
-            String navn = JOptionPane.showInputDialog("Navn på spiller " + (i + 1) + ":");
-            int fargeValg = JOptionPane.showOptionDialog(null, "Velg farge på brikke", "Brikkevalg",
-                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, muligeFarger.toArray(), muligeFarger.get(0));
-            String farge = muligeFarger.get(fargeValg);
-            muligeFarger.remove(fargeValg);
-
-            Spiller s = new Spiller(navn);
-            s.setBrikke(new Brikke(farge, brett));
-            spillere.add(s);
-        }
-
-        if (spillere.size() >= 2) {
-            spill.setSpillere(spillere);
-            spill.spill();
-        }
+        spill.spill();
 
 
     }
---
+++ src/driver/SpillerVelger.java
@@ -0,0 +1,52 @@
+package driver;
+
+import entities.Brett;
+import entities.Brikke;
+import entities.Spiller;
+import entities.Stigespill;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SpillerVelger {
+    private ArrayList<Spiller> spillere;
+    private Stigespill spill;
+    private Brett brett;
+    private final String[] ANTALL_MULIGE_SPILLERE = {"2", "3", "4"};
+    private final ArrayList<String> MULIGE_FARGER = new ArrayList<>(Arrays.asList("Rosa", "Sort", "Gul", "Brun"));
+
+    public SpillerVelger(Stigespill spill, Brett brett) {
+        spillere = new ArrayList<>();
+        this.spill = spill;
+        this.brett = brett;
+    }
+
+
+    public void velgSpillere() {
+
+        int valgtAntallSpillere = JOptionPane.showOptionDialog(null, "Velg antall spillere", "Antall spillere",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, ANTALL_MULIGE_SPILLERE, ANTALL_MULIGE_SPILLERE[0]);
+
+        int antallSpillere = Integer.parseInt(ANTALL_MULIGE_SPILLERE[valgtAntallSpillere]);
+
+        for (int i = 0; i < antallSpillere; i++) {
+            String navn = JOptionPane.showInputDialog("Navn på spiller " + (i + 1) + ":");
+            int fargeValg = JOptionPane.showOptionDialog(null, "Velg farge på brikke", "Brikkevalg",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, MULIGE_FARGER.toArray(), MULIGE_FARGER.get(0));
+
+            String farge = MULIGE_FARGER.get(fargeValg);
+            MULIGE_FARGER.remove(fargeValg);
+
+            Spiller s = new Spiller(navn);
+            s.setBrikke(new Brikke(farge, brett));
+            spillere.add(s);
+        }
+
+        if(spillere.size() >= 2){
+            spill.setSpillere(spillere);
+        }
+    }
+
+}
