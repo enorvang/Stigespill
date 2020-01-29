1f2b84a05e47536fbd0e38d35453569cfa84639d
enorvang <espen.norvang@gmail.com>
2020-01-29 09:22:50 +0100

    fikset formatering

--- src/driver/GameController.java
+++ src/driver/GameController.java
@@ -8,11 +8,9 @@ public class GameController {
 
     public static void main(String[] args) {
         Stigespill spill = new Stigespill();
-
         SpillerVelger sv = new SpillerVelger(spill);
 
         sv.velgSpillere();
-
         spill.spill();
 
 
